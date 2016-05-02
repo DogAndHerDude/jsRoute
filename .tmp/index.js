e342cd8d016a24202085b500bdb13f0360ba8f8d
Forestdev <dominykasbart@gmail.com>
2016-05-02 19:49:29 +0200

    Routing init && LocationWatcher

--- .tmp/index.js
+++ .tmp/index.js
@@ -1,13 +1,62 @@
 "use strict";
 ;
 (function () {
-    var jsRoute = (function () {
-        function jsRoute() {
+    var noop = function () { return null; };
+    var routes = [];
+    var Route = (function () {
+        function Route(path, options) {
+            var self = this;
+            self.path = path;
+            self.options = options;
         }
-        jsRoute.prototype.when = function (path, options) {
-        };
-        return jsRoute;
+        return Route;
     }());
-    window['jsRoute'] = window['jsRoute'] || new jsRoute();
+    var LocationWatcher = (function () {
+        function LocationWatcher() {
+            var self = this;
+            self.registerListeners();
+        }
+        LocationWatcher.prototype.registerListeners = function () {
+            var self = this;
+            window.addEventListener('load', function () {
+                self.history = window.history;
+                self.location = window.location;
+                self.rootElement = document.querySelector('.jsroute-view');
+                self.preventRootClick();
+            }, false);
+        };
+        LocationWatcher.prototype.preventRootClick = function () {
+            var self = this;
+            self.rootElement.addEventListener('click', function (ev) {
+                if (ev.target.nodeName === "A") {
+                    ev.preventDefault();
+                }
+            }, false);
+        };
+        LocationWatcher.prototype.updateURL = function () {
+        };
+        LocationWatcher.prototype.onChange = function (callback) {
+            var self = this;
+        };
+        return LocationWatcher;
+    }());
+    function run() {
+        var jsRoute = {};
+        var watcher = new LocationWatcher();
+        watcher.onChange(function (ev) {
+            console.log(ev);
+            ev.preventDefault();
+        });
+        history.pushState({ foo: 'bar' }, "page2", "index.html");
+        window.onpopstate = function (ev) {
+            console.log(ev);
+        };
+        function when(path, options) {
+            routes.push(new Route(path, options));
+        }
+        return jsRoute;
+    }
+    var jsRoute = run();
+    window['jsRoute'] = window['jsRoute'] || jsRoute;
 })();
 //# sourceMappingURL=../src/tmp/maps/index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,91 @@
 "use strict";
 
 ;(function() {
-  /*
-   * Interfaces
-   */
+
+  var noop = function() { return null; }
+  var routes: Array<Route> = [];
 
   interface RouteOptions {
     templateUrl: string;
+    //onLoad(): void;
   }
 
-  interface RouteProvider {
-    when(path: string, options: RouteOptions): void;
-  }
+  class Route {
+    public path: string;
+    public options: RouteOptions;
 
-  class jsRoute implements RouteProvider {
-    private routes: Array<any>;
+    constructor(path: string, options: RouteOptions) {
+      var self = this;
 
-    public when(path: string, options: RouteOptions): void {
-
+      self.path = path;
+      self.options = options;
     }
   }
 
-  window['jsRoute'] = window['jsRoute'] || new jsRoute();
+  class LocationWatcher {
+    private location;
+    private history;
+    private rootElement;
+
+    constructor() {
+      var self = this;
+
+      self.registerListeners();
+    }
+
+    private registerListeners(): void {
+      var self = this;
+
+      window.addEventListener('load', () => {
+        self.history = window.history;
+        self.location = window.location;
+        self.rootElement = document.querySelector('.jsroute-view');
+
+        self.preventRootClick();
+      }, false);
+    }
+
+    private preventRootClick(): void {
+      var self = this;
+
+      self.rootElement.addEventListener('click', (ev) => {
+        if(ev.target.nodeName === "A") {
+          ev.preventDefault();
+        }
+      }, false);
+    }
+
+    private updateURL() {
+
+    }
+
+    public onChange(callback): void {
+      var self = this;
+    }
+  }
+
+  function run() {
+    var jsRoute = {};
+    var watcher = new LocationWatcher();
+
+    watcher.onChange(function(ev) {
+      console.log(ev);
+      ev.preventDefault();
+    });
+
+    history.pushState({ foo: 'bar' }, "page2", "index.html");
+    window.onpopstate = function(ev) {
+      console.log(ev);
+    }
+
+    function when(path: string, options: RouteOptions) {
+      routes.push(new Route(path, options));
+    }
+
+    return jsRoute;
+  }
+
+  let jsRoute = run();
+
+  window['jsRoute'] = window['jsRoute'] || jsRoute;
 })();
